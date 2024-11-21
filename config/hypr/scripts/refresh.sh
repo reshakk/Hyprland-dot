3efe02d26e6e3bdb77ffae3a68f23ec910e009ca
reshakk <140834027+reshakk@users.noreply.github.com>
2024-11-21 22:31:54 +0800

    Add files via upload

---
+++ config/hypr/scripts/refresh.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Kill already running processes
+_ps=(waybar swaync hypridle rofi)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+sleep 0.2
+# Relaunch waybar
+waybar > /dev/null 2>&1 &
+
+# relaunch swaync
+sleep 0.2
+swaync > /dev/null 2>&1 &
+
+
+# relaunch swaync
+sleep 0.2
+hypridle > /dev/null 2>&1 &
+
+exit 0
